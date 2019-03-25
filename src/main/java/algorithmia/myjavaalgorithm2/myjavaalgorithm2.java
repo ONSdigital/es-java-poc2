93b4b5b30d892e094e9552b567ce6aa87b1642fe
Ashutosh Jindal <Ash.Jindal@ext.ons.gov.uk>
2019-03-25 12:20:38 +0000

    Add SpotBugs for static analysis as FindBugs is no longer maintained
    As seen on https://gleclaire.github.io/findbugs-maven-plugin/ findbugs
    is no longer maintained and the recommendation is to use SpotBugs.
    
    SpotBugs reports are supported out of the box with SonarQube. Once
    generated the reports can be imported via:
    
    ```
    -Dsonar.java.spotbugs.reportPaths="./target/spotbugsXml.xml"
    ```
    
    (assuming the default path for SpotBugs report)
    
    This commit includes a 'bug' (Unused field) to verify detection in the
    final report generated by SonarQube

--- src/main/java/algorithmia/myjavaalgorithm2/myjavaalgorithm2.java
+++ src/main/java/algorithmia/myjavaalgorithm2/myjavaalgorithm2.java
@@ -7,6 +7,10 @@ import com.google.gson.*;
 import java.util.*;
 
 public class myjavaalgorithm2 {
+    //Add a bug to verify Spotbug report
+    // Unused field
+    private int spotBugsShouldFlagThis;
+
     public String apply(String s) throws Exception {
         return "Hello " + s;
     }
